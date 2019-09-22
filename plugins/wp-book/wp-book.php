78f0f46d8c51d7c2befb36ebf743194d5be1e30d
Vishwesh Soni <vishweshsoni@gmail.com>
2019-09-23 03:20:21 +0530

    adding plugin info

---
+++ plugins/wp-book/wp-book.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * RtCamp Task one Plugin 
+ * 
+ * This Plugin will have the Book custom plugin and custom ,
+ * texonomy and also also working woth metabox ,metadata API,
+ * and extension of metadata API
+ * 
+ * @link http://example.com/
+ * @since 1.0.0
+ * @package wp-book
+ * 
+ * @worpress-plugin
+ * Plugin Name:       wp-book
+ * Plugin URI:        http://example.com/p
+ * Description:       RtCamp task-1 plugin 1
+ * Version:           1.0.0
+ * Author:            Vishwesh Soni 
+ * Author URI:        https://github.com/vishweshsoni
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wp-book
+ */
